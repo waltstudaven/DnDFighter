6838466be7a2f554bf4f141599aecb89386dab03
Katie Spiese <katiespiese@pugetsound.edu>
2018-10-24 13:44:57 -0700

    commenting
    comment

--- CharacterClass.java
+++ CharacterClass.java
@@ -3,6 +3,10 @@ public abstract class CharacterClass {
     int profBonus;
     Die hitDie;
 
+    /**
+    * Sets the prof bonus for based on level
+    * @param level The level
+    */
     public void setProfBonusViaLevel(int level) {
         if (level < 5) setProfBonus(2);
         else if (level < 9 && level >= 5) setProfBonus(3);
@@ -13,13 +17,38 @@ public abstract class CharacterClass {
             System.out.println("Not a valid level");
         }
     }
+
+    /**
+    * Sets the prof bonus for the character
+    * @param profBonus The prof bonus
+    */
     public void setProfBonus(int profBonus) { this.profBonus = profBonus; }
+
+    /**
+    * Returns the prof bonus
+    * @return The prof bonus
+    */
     public int getProfBonus() { return profBonus; }
 
+    /**
+    * Sets the level for the character
+    * @param level The level
+    */
     public void setLevel(int level) { this.level = level; }
+
+    /**
+    * Returns the level
+    * @return The level
+    */
     public int getLevel() { return this.level; }
 
+    /**
+    * Sets the saved throws 
+    */
     public abstract void setSaveThrowsProf();
-    
+
+    /**
+    * Determines the health
+    */
     public abstract void determineHealth();
 }
