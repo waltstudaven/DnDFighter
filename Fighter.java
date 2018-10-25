daf2584a82cc245d2489f4726058037b6019a1e4
Walter Stuart Davenport <sdavenport@ups.edu>
2018-10-25 11:37:09 -0700

    Fighter update
    probabaly gonna be lots of problems

--- Fighter.java
+++ Fighter.java
@@ -11,6 +11,29 @@ Character thisCharacter;
     @Override
     private void setLvlOne() {
       thisCharacter.setStrSaveProf();
-      thisCharacter.setDexSaveProf();
+      thisCharacter.setConSaveProf();
+    }
+
+    private void setSkills(String skill1, String skill2) {
+    }
+
+    private void setFightStyle(String fightStyle) {
+        if (fightStyle.equalsIgnoreCase("archery")) {
+            if (thisCharacter.Weapon.Ranged()) thisCharacter.Weapon.attackRole = thisCharacter.Weapon.attackRole +2;
+        }
+        else if (fightStyle.equalsIgnoreCase("defense")) thisCharacter.setAc(thisCharacter.getAc() +1);
+        else if (fightStyle.equalsIgnoreCase("dueling")) {
+          if (thisCharacter.numEquiptWeapon == 1 && thisCharacter.Weapon.melee()) {
+            thisCharacter.Weapon.rollDamage() = thisCharacter.Weapon.rollDamage() + 2;
+          }
+        }
+        else if (fightStyle.equalsIgnoreCase("Great Weapon Fighting")) {
+          if (thisCharacter.Weapon.twoHanded() || thisCharacter.Weapon.versatile() && thisCharacter.numEquiptWeapon == 2) {
+            if (thisCharacter.Weapon.rollDamage() == 1 || thisCharacter.Weapon.rollDamage() == 2) {
+              thisCharacter.Weapon.rollDamage() = thisCharacter.Weapon.rollDamage();
+            }
+          }
+        }
+
     }
   }
