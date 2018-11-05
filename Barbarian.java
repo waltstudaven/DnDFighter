82cbc57d9a30e601b51cf30098322d6c944399b4
JHobbs96 <43074826+JHobbs96@users.noreply.github.com>
2018-11-05 11:10:46 -0800

    Update Barbarian.java

--- Barbarian.java
+++ Barbarian.java
@@ -1,26 +1,36 @@
-// public class Barabarian extends CharacterClass{
-//   int rages;
-//   int rageDmg;
-//   boolean raging;
-//   public Barabrian(int level, Die hitDie) {
-//     super.level = level;
-//     super.profBonus = setProfBonusViaLevel(level);
-//     super.hitDie = hitDie;
-//     raging = false;
-//   }
-//
-//   public void setLvl(int level){
-//     if(level >= 1) setLvlOne();
-//   }
-//
-//   public void setLvlOne() {
-//     rages = 2;
-//     rageDmg = 2;
-//     if (raging)
-//   }
-//
-//   public void rage() {
-//     raging = true;
-//
-//   }
-// }
+/**
+ * Barbarian is a character's class that will determine a character's
+ * skills, attacks, and health.
+ */
+public class Barabarian extends CharacterClass{
+   int rages; 
+   int rageDmg;
+   boolean raging;
+   public Barabrian(int level, Die hitDie) {
+     super.level = level;
+     super.profBonus = setProfBonusViaLevel(level);
+     super.hitDie = hitDie;
+     raging = false;
+   }
+   /**
+    * Determines the strength and other bonuses based off of character's level.
+    */
+   public void setLvl(int level){
+     if(level >= 1) setLvlOne();
+   }
+   /**
+    * Determines the strength and other bonuses for level 1 character.
+    */
+   public void setLvlOne() {
+     rages = 2;
+     rageDmg = 2;
+     if (raging)
+   }
+   /**
+    * Uses the Barbarian rage.
+    */
+   public void rage() {
+     raging = true;
+
+   }
+ }
