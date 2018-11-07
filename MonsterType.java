5e91aa7b3011d0191be083878a62d9b74e425b93
JHobbs96 <43074826+JHobbs96@users.noreply.github.com>
2018-11-06 16:17:21 -0800

    Update MonsterType.java

--- MonsterType.java
+++ MonsterType.java
@@ -1,3 +1,6 @@
+/**
+ * This enum restricts and keeps track of different monster types.
+ */
 public enum MonsterType {
     ABERRATION, BEAST, CELESTIAL, CONSTRUCT, DRAGON, ELEMENTAL, FEY, FEIND, GIANT, HUMANOID, MONSTROSITY, OOZE, PLANT, UNDEAD;
 }
