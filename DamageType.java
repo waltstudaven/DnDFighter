ed4094f281e509c75ec284ba0ba78d6b9cfff1b5
JHobbs96 <43074826+JHobbs96@users.noreply.github.com>
2018-11-06 16:15:46 -0800

    Update DamageType.java

--- DamageType.java
+++ DamageType.java
@@ -1,3 +1,7 @@
+/**
+ * This enum keeps track of the Damage Type that an attack performs.
+ * Attacks must contain a Damage Type.
+ */
 public enum DamageType {
 	ACID("acid"), BLUDGEONING("bludgeoning"), COLD("cold"), FIRE("fire"), FORCE("force"),
 	LIGHTNING("lightning"), NECROTIC("necrotic"), PIERCING("piercing"), POISON("poison"), PSYCHIC("psychic"),
